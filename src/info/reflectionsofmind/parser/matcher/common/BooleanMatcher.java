5c30e7fb46ba145e236a4436f913a45743133488
emorning <emorning@e37ea728-f2ec-11dd-8c4e-4bbb75a408b7>
2009-04-15 16:12:26 +0000

    Move custom matchers that I created to a package just for holding common matchers that other developers may reuse

--- src/info/reflectionsofmind/parser/matcher/common/BooleanMatcher.java
+++ src/info/reflectionsofmind/parser/matcher/common/BooleanMatcher.java
@@ -1,27 +1,29 @@
-package info.reflectionsofmind.parser.matcher;
+package info.reflectionsofmind.parser.matcher.common;
 
 import info.reflectionsofmind.parser.Result;
+import info.reflectionsofmind.parser.matcher.Matcher;
 import info.reflectionsofmind.parser.node.StringNode;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class IntegerMatcher
+public class BooleanMatcher
 implements Matcher
 {
 	@Override
 	public List<Result> match(String input)
 	{
-		char c= input.charAt(0);
-		int i= (c == '+' || c == '-') ? 1 : 0;
-		
-		if (!Character.isDigit(input.charAt(i++)))
+		int i= 0;
+		if (input.startsWith("true")) {
+			i= 4;
+		}
+		else if (input.startsWith("false"))
+			i= 5;
+
+		if (i <= 0) 
 			return Collections.<Result> emptyList();
-		
-		while (Character.isDigit(input.charAt(i)))
-			i++;
-		
+
 		return Arrays.asList(new Result(new StringNode(input.substring(0, i)), i));
 	}
 }
--- src/info/reflectionsofmind/parser/matcher/common/IntegerMatcher.java
+++ src/info/reflectionsofmind/parser/matcher/common/IntegerMatcher.java
@@ -1,28 +1,31 @@
-package info.reflectionsofmind.parser.matcher;
+package info.reflectionsofmind.parser.matcher.common;
 
 import info.reflectionsofmind.parser.Result;
+import info.reflectionsofmind.parser.matcher.Matcher;
 import info.reflectionsofmind.parser.node.StringNode;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class BooleanMatcher
+public class IntegerMatcher
 implements Matcher
 {
 	@Override
 	public List<Result> match(String input)
 	{
-		int i= 0;
-		if (input.startsWith("true")) {
-			i= 4;
-		}
-		else if (input.startsWith("false"))
-			i= 5;
-
-		if (i <= 0) 
+		if (input.length() <= 0)
 			return Collections.<Result> emptyList();
-
+		
+		char c= input.charAt(0);
+		int i= (c == '+' || c == '-') ? 1 : 0;
+		
+		if (!Character.isDigit(input.charAt(i++)))
+			return Collections.<Result> emptyList();
+		
+		while (Character.isDigit(input.charAt(i)))
+			i++;
+		
 		return Arrays.asList(new Result(new StringNode(input.substring(0, i)), i));
 	}
 }
