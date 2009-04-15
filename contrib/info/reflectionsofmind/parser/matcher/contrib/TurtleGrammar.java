a8dcab5a4c8a1b2c0ed7215cd3710600ed4f83b9
emorning <emorning@e37ea728-f2ec-11dd-8c4e-4bbb75a408b7>
2009-04-15 16:10:59 +0000

    Create a new source folder for holding contributed grammars. Add Turtle Grammer. Also added test for Turtle grammar to tests folder.

---
+++ contrib/info/reflectionsofmind/parser/matcher/contrib/TurtleGrammar.java
@@ -0,0 +1,158 @@
+package info.reflectionsofmind.parser.matcher.contrib;
+
+import static info.reflectionsofmind.parser.Parsers.cho;
+import static info.reflectionsofmind.parser.Parsers.longs;
+import static info.reflectionsofmind.parser.Parsers.min;
+import static info.reflectionsofmind.parser.Parsers.mins;
+import static info.reflectionsofmind.parser.Parsers.opt;
+import static info.reflectionsofmind.parser.Parsers.opts;
+import static info.reflectionsofmind.parser.Parsers.range;
+import static info.reflectionsofmind.parser.Parsers.reps;
+import static info.reflectionsofmind.parser.Parsers.seq;
+import static info.reflectionsofmind.parser.Parsers.str;
+import info.reflectionsofmind.parser.Result;
+import info.reflectionsofmind.parser.matcher.Matcher;
+import info.reflectionsofmind.parser.matcher.NamedMatcher;
+import info.reflectionsofmind.parser.matcher.common.BooleanMatcher;
+import info.reflectionsofmind.parser.matcher.common.DecimalMatcher;
+import info.reflectionsofmind.parser.matcher.common.DoubleMatcher;
+import info.reflectionsofmind.parser.matcher.common.IntegerMatcher;
+import info.reflectionsofmind.parser.matcher.common.QuotedStringMatcher;
+import info.reflectionsofmind.parser.matcher.common.WhitespaceMatcher;
+import info.reflectionsofmind.parser.node.StringNode;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Defines a grammer for the TURTLE RDF language.
+ * @see http://www.dajobe.org/2004/01/turtle/
+ * 
+ * Use TurtleGrammar.getMatcher() to get a matcher for parsing a Turtle RDF document.
+ * 
+ * @author Ted Stockwell [emorning@yahoo.com]
+ */
+public class TurtleGrammar
+{
+	/**
+	 * Symbols used in this grammar
+	 */
+	public static final String DOCUMENT = "turtleDocument";
+	public static final String STATEMENT = "statement";
+	public static final String DIRECTIVE = "directive";
+	public static final String TRIPLES = "triples";
+	public static final String PREDICATE_OBJECT_LIST = "predicateObjectList";
+	public static final String VERB = "verb";
+	public static final String OBJECT_LIST = "objectList";
+	public static final String OBJECT = "object";
+	public static final String LITERAL = "literal";
+	public static final String LANGUAGE = "language";
+	public static final String DATATYPE_STRING = "datatypeString";
+	public static final String INTEGER = "integer";
+	public static final String DOUBLE = "double";
+	public static final String DECIMAL = "decimal";
+	public static final String BOOL = "bool";
+	public static final String PREFIX_ID = "prefixID";
+	public static final String BASE = "base";
+	public static final String SUBJECT = "subject";
+	public static final String RESOURCE = "resource";
+	public static final String URIREF = "uriref";
+	public static final String PREFIX_NAME = "prefixName";
+	public static final String NODE_ID = "nodeID";
+	public static final String QNAME = "qname";
+	public static final String QUOTED_STRING = "quotedString";
+
+	public static class URIRefMatcher implements Matcher 
+	{
+		@Override 
+		public List<Result> match(String input)
+		{
+			if (input.length() <= 0 || input.charAt(0) != '<')
+				return Collections.<Result> emptyList();
+			
+			int pos = input.indexOf('>', 1);
+			if (pos < 0)
+				return Collections.<Result> emptyList();
+			
+			pos++;
+			String text= input.substring(0, pos);
+			return Arrays.asList(new Result(new StringNode(text), pos));
+		}
+	}
+	
+	public static class NameMatcher implements Matcher 
+	{
+		@Override 
+		public List<Result> match(String input)
+		{
+			int len= input.length();
+			if (len <= 0)
+				return Collections.<Result> emptyList();
+			char ch= input.charAt(0);
+			if (!Character.isLetter(ch) || Character.isWhitespace(ch))
+				return Collections.<Result> emptyList();
+			int i= 1;
+			for (; i < len; i++) {
+				ch= input.charAt(i);
+				if (!Character.isLetterOrDigit(ch) || Character.isWhitespace(ch))
+					if (ch != '-')
+						break;
+			}
+			return Arrays.asList(new Result(new StringNode(input.substring(0, i)), i));
+		}
+	}
+	
+	static HashMap<String, Matcher> __matchersById= new HashMap<String, Matcher>(); 
+	
+	
+	
+	static {
+		Matcher lower = range('a', 'z');
+		Matcher digit = range('0', '9');
+		Matcher ws = new WhitespaceMatcher();
+		Matcher optws = opt(ws);
+		Matcher name= new NameMatcher();
+		
+		NamedMatcher quotedString= defineMatcher(QUOTED_STRING, new QuotedStringMatcher());
+		NamedMatcher qname= defineMatcher(QNAME, longs(opt(name), str(":"), opt(name)));
+		NamedMatcher nodeID= defineMatcher(NODE_ID, seq(str("_:"), name));
+		NamedMatcher prefixName= defineMatcher(PREFIX_NAME, name);
+		NamedMatcher uriref= defineMatcher(URIREF, new URIRefMatcher());
+		NamedMatcher resource= defineMatcher(RESOURCE, cho(uriref, qname));
+		NamedMatcher subject= defineMatcher(SUBJECT, cho(resource, nodeID, str("[]")));
+		NamedMatcher base= defineMatcher(BASE, seq(str("@base"), ws, uriref));
+		NamedMatcher prefixID= defineMatcher(PREFIX_ID, seq(str("@prefix"), ws, opt(prefixName), str(":"), ws, uriref));
+		NamedMatcher bool= defineMatcher(BOOL, new BooleanMatcher());
+		NamedMatcher decimal= defineMatcher(DECIMAL, new DecimalMatcher());
+		NamedMatcher doubl= defineMatcher(DOUBLE, new DoubleMatcher());
+		NamedMatcher integer= defineMatcher(INTEGER, new IntegerMatcher());
+		NamedMatcher datatypeString= defineMatcher(DATATYPE_STRING, seq(quotedString, str("^^"), resource));
+		NamedMatcher language= defineMatcher(LANGUAGE, seq(min(1, lower), reps(str("-"), min(1, seq(cho(lower, digit))))));
+		NamedMatcher literal= defineMatcher(LITERAL, cho(seq(quotedString, opts(str("@"), language)), datatypeString, integer, doubl, decimal, bool));
+		NamedMatcher object= defineMatcher(OBJECT, cho(resource, literal, nodeID, str("[]")));
+		NamedMatcher objectList= defineMatcher(OBJECT_LIST, seq(object, reps(optws, str(","), optws, object)));
+		NamedMatcher verb= defineMatcher(VERB, cho(resource, str("a")));
+		NamedMatcher predicateObjectList= defineMatcher(PREDICATE_OBJECT_LIST, longs(verb, ws, objectList, reps(optws, str(";"), optws, verb, ws, objectList)));
+		NamedMatcher triples= defineMatcher(TRIPLES, seq(subject, ws, predicateObjectList));
+		NamedMatcher directive= defineMatcher(DIRECTIVE, cho(prefixID, base));
+		NamedMatcher statement= defineMatcher(STATEMENT, cho(seq(directive, optws, str(".")), seq(triples, optws, str("."))));
+		defineMatcher(DOCUMENT, longs(mins(1, optws, statement), optws));
+	}
+	
+	private static NamedMatcher defineMatcher(String symbol, Matcher definition) {
+		NamedMatcher matcher= new NamedMatcher(symbol).define(definition);
+		__matchersById.put(symbol, matcher);
+		return matcher;
+	}
+	
+	public static Matcher getMatcher(String symbol) {
+		return __matchersById.get(symbol);
+	}
+	public static Matcher getMatcher() {
+		return getMatcher(DOCUMENT);
+	}
+	
+	
+}
---
+++ test/info/reflectionsofmind/parser/TurtleTests.java
@@ -0,0 +1,82 @@
+package info.reflectionsofmind.parser;
+
+import static info.reflectionsofmind.parser.matcher.contrib.TurtleGrammar.DOCUMENT;
+import static info.reflectionsofmind.parser.matcher.contrib.TurtleGrammar.LITERAL;
+import static info.reflectionsofmind.parser.matcher.contrib.TurtleGrammar.OBJECT_LIST;
+import static info.reflectionsofmind.parser.matcher.contrib.TurtleGrammar.PREDICATE_OBJECT_LIST;
+import static info.reflectionsofmind.parser.matcher.contrib.TurtleGrammar.STATEMENT;
+import static info.reflectionsofmind.parser.matcher.contrib.TurtleGrammar.SUBJECT;
+import static info.reflectionsofmind.parser.matcher.contrib.TurtleGrammar.TRIPLES;
+import static info.reflectionsofmind.parser.matcher.contrib.TurtleGrammar.URIREF;
+import static info.reflectionsofmind.parser.matcher.contrib.TurtleGrammar.VERB;
+import info.reflectionsofmind.parser.matcher.Matcher;
+import info.reflectionsofmind.parser.matcher.Matchers;
+import info.reflectionsofmind.parser.matcher.contrib.TurtleGrammar;
+import info.reflectionsofmind.parser.node.AbstractNode;
+import info.reflectionsofmind.parser.node.Navigation;
+
+import java.util.List;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class TurtleTests
+extends TestCase
+{
+	public void testBasicTriples() throws Exception {
+		
+		String document= 	
+			"@prefix resource: <meteor:net.sf.meteor.Resource.>.\n" +
+			"@prefix test: <meteor:net.sf.meteor.test.>.\n" +
+			"\n" +
+			"test:CustomerFromManifest resource:type test:Customer ;\n" +
+			"			resource:description \"A Customer defined in a Meteor manifest\" .\n\n\n\n\n";
+		
+		Matcher literalMatcher= TurtleGrammar.getMatcher(LITERAL);		
+		Assert.assertEquals(1, literalMatcher.match("\"A Customer defined in a Meteor manifest\"").size());
+		
+		String testgrammar= "uriref ::= (\"<\" relativeURI \">\")\n" +
+				"relativeURI ::= (anychar {anychar})\n" +
+				"anychar ::= (%alpha% | %digit% | \" \" | \":\" | \".\")";  
+		Matcher relativeURIMatcher= Grammar.generate(testgrammar, "relativeURI");		
+		Assert.assertEquals(1, Matchers.fullMatch(relativeURIMatcher, "meteor:net.sf.meteor.TestBindType.instance").size());
+		
+		Matcher urirefMatcher= TurtleGrammar.getMatcher(URIREF);		
+		Assert.assertEquals(1, urirefMatcher.match("<meteor:net.sf.meteor.TestBindType.instance>").size());
+		
+		Matcher subjectMatcher= TurtleGrammar.getMatcher(SUBJECT);		
+		Assert.assertEquals(1, subjectMatcher.match("<meteor:net.sf.meteor.TestBindType.instance>").size());
+		Assert.assertEquals(1, subjectMatcher.match("test:CustomerFromManifest").size());
+		
+		Matcher verbMatcher= TurtleGrammar.getMatcher(VERB);	
+		Assert.assertEquals(1, verbMatcher.match("resource:type").size());
+		
+		Matcher objectListMatcher= TurtleGrammar.getMatcher(OBJECT_LIST);		
+		Assert.assertEquals(1, objectListMatcher.match("webbench:WebbenchModule").size());
+		
+		Matcher predicateObjectListMatcher= TurtleGrammar.getMatcher(PREDICATE_OBJECT_LIST);		
+		Assert.assertEquals(1, predicateObjectListMatcher.match("resource:type webbench:WebbenchModule").size());
+		Assert.assertEquals(1, predicateObjectListMatcher.match("resource:description \"A Customer defined in a Meteor manifest\"").size());
+		Assert.assertEquals(1, predicateObjectListMatcher.match("resource:type test:Customer ;\n\t\t\tresource:description \"A Customer defined in a Meteor manifest\"").size());
+		
+		Matcher triplesMatcher= TurtleGrammar.getMatcher(TRIPLES);
+		String tripleDoc= "<meteor:net.sf.meteor.TestBindType.instance> resource:type webbench:WebbenchModule"; 
+		Assert.assertEquals(1, triplesMatcher.match(tripleDoc).size());
+		tripleDoc= "test:CustomerFromManifest resource:type test:Customer ;\n\t\t\tresource:description \"A Customer defined in a Meteor manifest\"";
+		Assert.assertEquals(1, triplesMatcher.match(tripleDoc).size());
+		
+		Matcher statementMatcher= TurtleGrammar.getMatcher(STATEMENT);
+		String statementDoc= tripleDoc+" .\n\n\n\n\n"; 
+		Assert.assertEquals(1, statementMatcher.match(statementDoc).size());
+		
+		Matcher matcher= TurtleGrammar.getMatcher(DOCUMENT);		
+		List<Result> results= matcher.match(document);
+		Assert.assertEquals(1, results.size());
+		AbstractNode rootNode= results.get(0).node;
+		
+		// the AST should have 3 statements in it
+		List<AbstractNode> statements= Navigation.findAllDecendentsById(rootNode, "statement");
+		Assert.assertEquals(3, statements.size());
+	}
+	
+}
