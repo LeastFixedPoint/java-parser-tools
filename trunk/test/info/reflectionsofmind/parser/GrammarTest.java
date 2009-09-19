75e2f0a5eb8cc3ede7be8c0527b8325bab3a1aa0
shooshpanchick <shooshpanchick@e37ea728-f2ec-11dd-8c4e-4bbb75a408b7>
2009-09-19 20:39:59 +0000

    More tests and comments.

--- trunk/test/info/reflectionsofmind/parser/GrammarTest.java
+++ trunk/test/info/reflectionsofmind/parser/GrammarTest.java
@@ -1,25 +1,30 @@
-/**
- * 
- */
 package info.reflectionsofmind.parser;
 
 import info.reflectionsofmind.parser.exception.GrammarParsingException;
+import info.reflectionsofmind.parser.exception.InvalidGrammarException;
+import info.reflectionsofmind.parser.exception.UndefinedSymbolException;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 public class GrammarTest
 {
-	@Test
-	public void grammarShouldDetectMissingSymbols()
+	@Test(expected = UndefinedSymbolException.class)
+	public void shouldThrowExceptionOnUndefinedSymbols() throws GrammarParsingException
 	{
-		String grammar = "a ::= (b c)\nb ::= %digit%";
-		try
-		{
-			Grammar.generate(grammar, "a");
-			Assert.fail("The Grammar.generate method failed to detect that the 'c' symbol is not defined.");
-		} catch (GrammarParsingException e)
-		{
-		}
+		String grammar = "a ::= (b c) b ::= %digit%";
+		Grammar.generate(grammar, "a");
+	}
+	
+	@Test
+	public void shouldAllowAnyNumberOfSpaces() throws GrammarParsingException
+	{
+		String grammar = "         a   ::=   ( b   b )           b   ::=  %digit%              ";
+		Grammar.generate(grammar, "a");
+	}
+	
+	@Test(expected = InvalidGrammarException.class)
+	public void shouldFailIfGrammarIsInvalid() throws GrammarParsingException
+	{
+		Grammar.generate("a ::= (%digi", "a");
 	}
 }
